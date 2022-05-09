88bd0e81e386484fe126349f9a48e80db72e69e3
Umair <umairkhan198@gmail.com>
2022-05-10 03:58:43 +0530

    PokeAPI:  fetch improved

--- poke/src/hooks/fetch.js
+++ poke/src/hooks/fetch.js
@@ -1,18 +1,29 @@
 import { useState, useEffect } from 'react';
+import { createBrowserHistory } from "history";
+import {createUrl} from '../util'
+
 
 function useFetch(url,limit) {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(null);
   const [navigation, setNavigation] = useState({next:"",previous:"",count:0});
+  let {location}  = createBrowserHistory();
 
   useEffect(() => {
     setLoading(true)
     const fetchData = async () => {
         try{
-            const initial = await fetch(url);
+            let searchParams = new URLSearchParams(location.search);
+            let count = searchParams.get('count');
+            limit = count ? count : limit;
+            let offset = searchParams.get('offset');
+            offset = offset?offset:0;
+            let newUrl =  createUrl(url,offset,limit)
+
+            const initial = await fetch(newUrl);
             const initialJson = await initial.json();
             
-            setNavigation({next :initialJson.next, previous :initialJson.previous, count:initialJson.count });
+            setNavigation({next :initialJson.next, previous :initialJson.previous, count:initialJson.count, offset,limit });
             const detailsData = initialJson.results.map(async (i) => {
                 const preFetchData = await fetch(i.url);
                 return preFetchData.json();
